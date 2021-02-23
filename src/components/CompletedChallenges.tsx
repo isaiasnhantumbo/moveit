504cbd53ee014a95f42f17865e7c651239d1aa32
Isaias Nhantumbo Junior <isaiasnhantumbo54@gmail.com>
2021-02-23 17:43:35 +0200

    :tada: added component CompChallenges with styles

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,11 @@
+
+import styles from '../styles/components/CompletedChallenges.module.css'
+
+export function CompletedChallenges(){
+    return (
+        <div className={styles.CompletedChallengesContainer}>
+            <span>Desafios Completos</span>
+            <span>8</span>
+        </div>
+    )
+}
