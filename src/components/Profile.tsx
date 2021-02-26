67434829d42cc01ccd2d500404bf8a092bb285e0
Isaias Nhantumbo Junior <isaiasnhantumbo54@gmail.com>
2021-02-26 19:35:53 +0200

    :tada: added Context

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,22 @@
-import styles from '../styles/components/Profile.module.css'
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
 
-export function Profile(){
-    return(
-        <div className={styles.profileContainer}>
-            <img src="https://github.com/isaiasnhantumbo.png" alt="Isaias Nhantumbo"/>
-            <div>
-                <strong>Isaias Nhantumbo</strong>
-                <p>
-                    <img src="icons/level.svg" alt=""/>
-                    Level 1</p>
-            </div>
-        </div>
-    )
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+  return (
+    <div className={styles.profileContainer}>
+      <img
+        src="https://github.com/isaiasnhantumbo.png"
+        alt="Isaias Nhantumbo"
+      />
+      <div>
+        <strong>Isaias Nhantumbo</strong>
+        <p>
+          <img src="icons/level.svg" alt="" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
 }
