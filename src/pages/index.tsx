bd96a30b56684c6810e46e94c65323b2400d17cb
Isaias Nhantumbo Junior <isaiasnhantumbo54@gmail.com>
2021-02-26 19:35:21 +0200

    :tad: added CountdownProvider to use Context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,27 +6,29 @@ import { ChallengeBox } from '../components/ChallengeBox';
 import styles from '../styles/pages/Home.module.css';
 
 import Head from 'next/head';
+import { CountdownProvider } from '../contexts/CountdownContext';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-
       <Head>
         <title>Inicio | move.it</title>
       </Head>
       <ExperienceBar />
 
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
 
-        <div>
-          <ChallengeBox/>
-        </div>
-      </section>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
   );
 }
